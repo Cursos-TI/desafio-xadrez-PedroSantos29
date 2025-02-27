724035cbd1ba5592d167eae9a1efda1457ffce70
PedroSantos29 <pedrosantos17@yahoo.com>
2025-02-27 19:21:08 +0000

    COMMIT MESTRE FINAL

--- xadreznovato.c
+++ xadreznovato.c
@@ -6,27 +6,42 @@
 #define CAVALO_MOV_X 2
 #define CAVALO_MOV_Y 1
 
+// Função recursiva para o movimento do Bispo
+void mover_bispo(int passos) {
+    if (passos == 0) return;
+    printf("Direita\n");
+    printf("Cima\n");
+    mover_bispo(passos - 1);
+}
+
+// Função recursiva para o movimento da Torre
+void mover_torre(int passos) {
+    if (passos == 0) return;
+    printf("Direita\n");
+    mover_torre(passos - 1);
+}
+
+// Função recursiva para o movimento da Rainha
+void mover_rainha(int passos) {
+    if (passos == 0) return;
+    printf("Esquerda\n");
+    mover_rainha(passos - 1);
+}
+
 int main() {
-    // Movimentação do Bispo (Diagonal Superior Direita)
+    // Movimentação do Bispo
     printf("Movimentação do Bispo:\n");
-    for (int i = 0; i < BISPO_MOV; i++) {
-        printf("Direita\n");
-        printf("Cima\n");
-    }
+    mover_bispo(BISPO_MOV);
     printf("\n");
 
-    // Movimentação da Torre (Direita)
+    // Movimentação da Torre
     printf("Movimentação da Torre:\n");
-    for (int i = 0; i < TORRE_MOV; i++) {
-        printf("Direita\n");
-    }
+    mover_torre(TORRE_MOV);
     printf("\n");
 
-    // Movimentação da Rainha (Esquerda)
+    // Movimentação da Rainha
     printf("Movimentação da Rainha:\n");
-    for (int i = 0; i < RAINHA_MOV; i++) {
-        printf("Esquerda\n");
-    }
+    mover_rainha(RAINHA_MOV);
     printf("\n");
 
     // Movimentação do Cavalo em L (Baixo e Esquerda) com loops aninhados (FOR e WHILE) 
